e7aeb156d9a9b57552c339aab24c68a7077b64ea
Caravaggio <felipe_797@hotmail.com>
2016-07-29 19:15:00 -0300

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,4 +1,4 @@
-import java.awt.*;
+import java.awt.*;		
 import java.net.*;
 import java.io.*;
 import java.util.ArrayList;
@@ -7,10 +7,13 @@ import java.util.List;
 
 public class Client {
 
+	public static final int VELOCIDADE = 10;
+	
 	public static void main(String[] args) {
 		
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		//System.out.println("Tamanho da tela: largura = " + dim.width + " e altura = " + dim.height);	
+		//System.out.println("Tamanho da tela: largura = " + dim.width + " e altura = " + dim.height);
+		
 		
 		try {
 
@@ -19,17 +22,22 @@ public class Client {
 			Robot robot = new Robot();
 			List<QuadradoTela> listaQuadrado = new ArrayList<QuadradoTela>();
 			
-			//Criação dos objetos das regiões da tela 
-			QuadradoTela quadrado1 = new QuadradoEsq(0, (dim.getHeight()-1)/3, (dim.getWidth()-1)/3, (dim.getHeight()-1)*2/3, dim);
-			QuadradoTela quadrado2 = new QuadradoDir( (dim.getWidth()-1)*2/3, (dim.getHeight()-1)/3, dim.getWidth()-1, (dim.getHeight()-1)*2/3, dim);
-			QuadradoTela quadrado3 = new QuadradoAcima((dim.getWidth()-1)/3, 0, (dim.getWidth()-1)*2/3, (dim.getHeight()-1)/3, dim);
-			QuadradoTela quadrado4 = new QuadradoAbaixo((dim.getWidth()-1)/3, (dim.getHeight()-1)*2/3, (dim.getWidth()-1)*2/3, dim.getHeight()-1, dim);
+			ThreadVelocidade thread = new ThreadVelocidade();
+			thread.start();
+			
+			
+			QuadradoTela quadrado1 = new QuadradoEsq(thread, 0, (dim.getHeight()-1)/3, (dim.getWidth()-1)/3, (dim.getHeight()-1)*2/3, dim);
+			QuadradoTela quadrado2 = new QuadradoDir(thread, (dim.getWidth()-1)*2/3, (dim.getHeight()-1)/3, dim.getWidth()-1, (dim.getHeight()-1)*2/3, dim);
+			QuadradoTela quadrado3 = new QuadradoAcima(thread, (dim.getWidth()-1)/3, 0, (dim.getWidth()-1)*2/3, (dim.getHeight()-1)/3, dim);
+			QuadradoTela quadrado4 = new QuadradoAbaixo(thread, (dim.getWidth()-1)/3, (dim.getHeight()-1)*2/3, (dim.getWidth()-1)*2/3, dim.getHeight()-1, dim);
 			
 			listaQuadrado.add(quadrado1);
 			listaQuadrado.add(quadrado2);
 			listaQuadrado.add(quadrado3);
 			listaQuadrado.add(quadrado4);
+			
 
+			
 			while(true){
 
 				int status = in.readInt();
@@ -39,17 +47,17 @@ public class Client {
 
 				System.out.println(status + " " + time + ": (" + x + ", " + y + ")");
 				
+				
 				Iterator<QuadradoTela> iter = listaQuadrado.iterator();
 				
 				int i = 1;
-				
-				//Verifica a lista das regiões da tela, se o ponteiro está em algum deles
+				//Verifica lista dos quadrados, se o ponteiro está em algum deles
 				while (iter.hasNext()) {
-					System.out.println("iterador: " + i);
+					System.out.println("iterador:" + i);
 					i++;
 					QuadradoTela aux = iter.next();
 					aux.ponteiroMouseAqui(x, y);
-					aux.movimentoMouse(robot);
+					aux.movimentoMouse();
 				}			
 				
 			}
