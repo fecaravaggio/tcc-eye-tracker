19cc5d79b00676d693edf72f67f70cccd64b73a8
Caravaggio <felipe_797@hotmail.com>
2016-07-29 19:22:16 -0300

    Update QuadradoDir.java

--- QuadradoDir.java
+++ QuadradoDir.java
@@ -1,20 +1,19 @@
 import java.awt.*;
-import java.awt.event.KeyEvent;
 
 public class QuadradoDir extends QuadradoTela {
 
-	public QuadradoDir(double posX, double posY, double posXFim, double posYFim, Dimension dimTela) {
+	public QuadradoDir(ThreadVelocidade thread, double posX, double posY, double posXFim, double posYFim, Dimension dimTela) {
 	
-		super(posX, posY, posXFim, posYFim, dimTela);
+		super(thread, posX, posY, posXFim, posYFim, dimTela);
 		
 	}
 
 	@Override
-	void movimentoMouse(Robot robot) {
+	void movimentoMouse() {
 		if (ponteiro == true)
-			robot.keyPress(KeyEvent.VK_RIGHT);
+			thread.velocidadeBotao(1, Client.VELOCIDADE);
 		else
-			robot.keyRelease(KeyEvent.VK_RIGHT);	
+			thread.velocidadeBotao(1, 0);	
 	}
 
 }
