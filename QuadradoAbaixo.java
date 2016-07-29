91de71adf0b07c252df480d77d6d7c86bbbfe2a5
Caravaggio <felipe_797@hotmail.com>
2016-07-29 19:18:02 -0300

    Update QuadradoAbaixo.java

--- QuadradoAbaixo.java
+++ QuadradoAbaixo.java
@@ -1,20 +1,20 @@
-import java.awt.*;
+import java.awt.*;	
 import java.awt.event.KeyEvent;
 
 public class QuadradoAbaixo extends QuadradoTela {
 
-	public QuadradoAbaixo(double posX, double posY, double posXFim, double posYFim, Dimension dimTela) {
+	public QuadradoAbaixo(ThreadVelocidade thread, double posX, double posY, double posXFim, double posYFim, Dimension dimTela) {
 
-		super(posX, posY, posXFim, posYFim, dimTela);
+		super(thread, posX, posY, posXFim, posYFim, dimTela);
 		
 	}
 
 	@Override
-	void movimentoMouse(Robot robot) {
+	void movimentoMouse() {
 		if (ponteiro == true)
-			robot.keyPress(KeyEvent.VK_S);
+			thread.velocidadeBotao(3, Client.VELOCIDADE);
 		else
-			robot.keyRelease(KeyEvent.VK_S);
+			thread.velocidadeBotao(3, 0);	
 		
 	}
 
